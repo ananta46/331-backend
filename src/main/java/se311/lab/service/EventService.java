22f18188b4949f9803dff31ccc78c3fc0fc9aa4c
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-09-20 01:37:52 +0700

    4.5

--- src/main/java/se311/lab/service/EventService.java
+++ src/main/java/se311/lab/service/EventService.java
@@ -1,12 +1,13 @@
 package se311.lab.service;
 
+import org.springframework.data.domain.Page;
 import se311.lab.entity.Event;
 
 import java.util.List;
 
 public interface EventService {
     Integer getEventSize();
-    List<Event> getEvents(Integer pageSize, Integer page);
+    Page<Event> getEvents(Integer pageSize, Integer page);
     Event getEvent(Long id);
 
 }
