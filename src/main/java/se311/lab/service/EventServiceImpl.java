a2bc87bde67101699105b29f75a34db79613640c
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-09-20 01:38:36 +0700

    4.6

--- src/main/java/se311/lab/service/EventServiceImpl.java
+++ src/main/java/se311/lab/service/EventServiceImpl.java
@@ -1,6 +1,7 @@
 package se311.lab.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import se311.lab.dao.EventDao;
 import se311.lab.entity.Event;
@@ -17,7 +18,7 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
         return eventDao.getEvents(pageSize, page);
     }
 
